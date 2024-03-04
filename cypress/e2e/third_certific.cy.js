f32eac478fb5f0d62e785351bd48c4d5b7665032
GermanPaul <german.kalmykov@mail.ru>
2024-03-04 14:42:37 +0300

    tyt

--- cypress/e2e/third_certific.cy.js
+++ cypress/e2e/third_certific.cy.js
@@ -1,14 +1,16 @@
-describe('Login and add deposit', () => {
-  it('test', () => {
-    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
-    cy.get('.btn-lg').contains('Customer Login').click();
-    cy.get('select').select('Ron Weasly');
-    cy.get('button').contains('Login').click();
-    cy.get('button').contains('Login').click();
-    cy.get('button[ng-class="btnClass2"]').click();
-    cy.get('input[placeholder="amount"]').type(1001);
+describe("Login and add deposit", () => {
+  it("test", () => {
+    cy.visit(
+      "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login"
+    );
+    cy.get("button").contains("Bank Manager Login").click();
+    cy.get("button").contains("Home").click();
+    cy.get("button").contains("Customer Login").click();
+    cy.get("select").select("Albus Dumbledore");
+    cy.get("button").contains("Login").click();
+    cy.get("button").contains("Deposit").click();
+    cy.get('input[placeholder="amount"]').type(100);
     cy.get('button[type="submit"]').click();
-    cy.get('span').contains('Deposit Successful');
-    cy.get('button').contains('Logout').click();
+    cy.get("button").contains("Logout").click();
   });
 });
